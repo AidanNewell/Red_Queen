46bcf81fc69515023e5de5212353fdf2194175d9
Aidan <am.newell@verizon.net>
2016-04-04 20:34:33 -0400

    Wowee this class sure looks complicated

---
+++ src/client/Core.java
@@ -0,0 +1,58 @@
+package client;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JPanel;
+
+public class Core extends JPanel implements KeyListener, MouseListener, Runnable{
+
+	
+	public void run()
+	{
+		
+	}
+
+	public void mouseClicked(MouseEvent arg0)
+	{
+		
+	}
+
+	public void mouseEntered(MouseEvent arg0)
+	{
+		
+	}
+
+	public void mouseExited(MouseEvent arg0)
+	{
+		
+	}
+
+	public void mousePressed(MouseEvent arg0)
+	{
+		
+	}
+
+	public void mouseReleased(MouseEvent arg0)
+	{
+		
+	}
+
+	public void keyPressed(KeyEvent arg0)
+	{
+		
+	}
+
+	public void keyReleased(KeyEvent arg0)
+	{
+		
+	}
+
+	public void keyTyped(KeyEvent arg0)
+	{
+		
+	}
+
+}
