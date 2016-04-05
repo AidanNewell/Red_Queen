02f5766c85ae367ae9d7d1b7ce8f1b826cd43433
Aidan <am.newell@verizon.net>
2016-04-04 21:34:09 -0400

    Menu things

--- src/menus/MainMenu.java
+++ src/menus/MainMenu.java
@@ -1,10 +1,15 @@
 package menus;
 
+import javax.swing.*;
+
 public class MainMenu extends MainGUI{
 
 	
 	public MainMenu()
 	{
-		setLayout();
+		setLayout(new BoxLayout( this, BoxLayout.Y_AXIS));
+		add(new JButton("New Game"));
+		add(new JButton("Options"));
+		add(new JButton("Quit"));
 	}
 }
