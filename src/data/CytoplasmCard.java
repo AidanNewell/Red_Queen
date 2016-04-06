ac60bb7584e15a7fe1d8fe79e938b39b50d677a8
Sahil Saini <sssaini011@gmail.com>
2016-04-05 21:08:01 -0400

    some stuff for CytoplasmCard

--- src/data/CytoplasmCard.java
+++ src/data/CytoplasmCard.java
@@ -2,12 +2,72 @@ package data;
 
 public class CytoplasmCard extends Card{
 	
-	
+	public CytoplasmCard(){
+		
+		int a = (int)(Math.random()*3);
+		
+		switch(a){
+		
+		case 0:
+			ATP = 1;
+			toxin = 1;
+			resistance = 2;
+			break;
+		case 1:
+			ATP = 2;
+			toxin = 2;
+			resistance = 2;
+			break;
+		case 2:
+			ATP = 0;
+			toxin = 0;
+			resistance = 3;
+			break;
+		
+		}
+		
+	}
 	
 	int getCardType() {
 		
 		return CYTOPLASM;
 		
+	}
+
+	
+	int getATP() {
+		
+		return ATP;
+	}
+
+	
+	void setATP(int a) {
+		ATP = a;
+		
+	}
+
+	
+	int getResistance() {
+		
+		return resistance;
+	}
+
+	
+	void setResistance(int r) {
+		
+		resistance = r;
+	}
+
+	
+	int getToxin() {
+		
+		return toxin;
+	}
+
+	
+	void setToxin(int t) {
+		
+		toxin = t;
 	}
 	
 
