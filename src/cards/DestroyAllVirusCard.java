e535abf28a3a69b2e16cae3064ac382bfe7ce9d3
Sahil Saini <sssaini011@gmail.com>
2016-04-15 13:08:01 -0400

    AND I GET UP AGAIN

--- src/cards/DestroyAllVirusCard.java
+++ src/cards/DestroyAllVirusCard.java
@@ -1,5 +1,23 @@
 package cards;
 
-public class DestroyAllVirusCard {
+import data.Alert;
+import data.Organism;
+import data.Player;
+
+public class DestroyAllVirusCard extends ActionCard{
+
+
+	Alert performAction(Organism o, Player p) {
+		
+		
+		
+		return null;
+	}
+
+	
+	int getCardType() {
+		
+		return Card.PETRI_DISH;
+	}
 
 }
