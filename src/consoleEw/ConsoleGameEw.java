bb43dc1f04ccb239cc2c54e0a4206cfd9d02c4fd
Sahil Saini <sssaini011@gmail.com>
2016-04-18 12:44:45 -0400

    more ew hey aidan get on ur shit

--- src/consoleEw/ConsoleGameEw.java
+++ src/consoleEw/ConsoleGameEw.java
@@ -3,5 +3,32 @@ package consoleEw;
 import data.Game;
 
 public class ConsoleGameEw extends Game{
+	
+	
+	public ConsoleGameEw(){
+		
+		
+		
+		
+	}
+	
+	
+	public void setUpGame(){
+		
+		
+	}
+	
+	
+	public void startGame(){
+		
+		
+	}
+
+
+	public static void main(String[] args){
+	
+		new ConsoleGameEw();
+	
+	}
 
 }
---
+++ src/consoleEw/ConsolePlayerEw.java
@@ -0,0 +1,84 @@
+package consoleEw;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import cards.CardLoader;
+import data.Hand;
+import data.Organism;
+import data.Player;
+
+public class ConsolePlayerEw extends Player{
+
+	private Hand hand;
+	private ArrayList<Organism> organisms; 
+	private int numCardsToDraw;
+	private int numOrganisms;
+	private String playerName;
+	
+	
+	public ConsolePlayerEw(){
+		
+		hand = new Hand();
+		organisms = new ArrayList<Organism>();
+		numOrganisms=4;
+		numCardsToDraw = 3;
+		Scanner s = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String w = s.nextLine();
+		playerName = w;
+		
+	}
+	
+	
+	public void drawCards(int a, int  b, int c){
+		for(int x=0; x<a; x++){
+			
+			hand.addCard(CardLoader.getOrganismCard());
+			
+		}
+		for(int x=0; x<b; x++){
+			
+			hand.addCard(CardLoader.getCytoplasmCard());
+			
+		}
+		for(int x=0; x<c; x++){
+			
+			hand.addCard(CardLoader.getPetriCard());
+			
+		}
+		
+	}
+	
+	public void updatePlayer()
+	{
+		numCardsToDraw=2;
+		for(Organism o : organisms)
+		{
+			int x = o.getCardsToDraw();
+			if(x > 0)
+				numCardsToDraw+=x;
+		}
+	}
+	
+	public int getNumOrganisms()
+	{
+		return organisms.size();
+	}
+	
+	public void newOrganism(Organism o)
+	{
+		organisms.add(o);
+	}
+	
+	public boolean isNewOrgValid()
+	{
+		return numOrganisms>=organisms.size();
+	}
+	
+	public void clearHand()
+	{
+		hand.getHand().clear();
+	}
+	
+}
