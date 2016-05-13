3d6e38768b3f173bb4b376a70b4a47a3e1efbbd9
Aidan Newell <am.newell@verizon.net>
2016-05-13 13:13:11 -0400

    THE KRAKEN! IT RETURNS!

--- src/client/OrganismPane.java
+++ src/client/OrganismPane.java
@@ -1,9 +1,13 @@
 package client;
 
+import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import cards.BuilderCard;
@@ -20,6 +24,7 @@ public class OrganismPane extends JPanel{
 		organism = o;
 		int width = organism.getWidth();
 		int height = organism.getHeight();
+		setLayout(new GridLayout(height,width));
 		int cardXSize = SPACE_AVAILABLE / width;
 		int cardYSize = SPACE_AVAILABLE / height;
 		for(int a=0; a<width;a++)
@@ -37,7 +42,18 @@ public class OrganismPane extends JPanel{
 						cardButton.setContentAreaFilled(false);
 						cardButton.setFocusPainted(false);
 						cardButton.setBorderPainted(false);
+						cardButton.addActionListener(new ActionListener(){
+							public void actionPerformed(ActionEvent e)
+							{
+								organism.getCardAt(a, b).activateCard();
+							}
+						});
 					}catch(Exception e){System.out.println("FATAL ERROR"); e.printStackTrace(); System.exit(Integer.MAX_VALUE);}
+					add(cardButton);
+				}
+				else
+				{
+					this.add(new JLabel());
 				}
 			}
 		}
