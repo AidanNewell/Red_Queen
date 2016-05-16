4ddd45c2f4dd1c36058d0f120fb404677f4fb140
Sahil Saini <sssaini011@gmail.com>
2016-05-16 18:48:53 -0400

    on god aidan if you dont pull before you see this

--- src/SahilAndAllisonsExcellentAdventure/AdventureGame.java
+++ src/SahilAndAllisonsExcellentAdventure/AdventureGame.java
@@ -1,5 +1,14 @@
 package SahilAndAllisonsExcellentAdventure;
 
-public class AdventureGame {
+import javax.swing.JFrame;
 
+public class AdventureGame {
+	
+	private JFrame gameFrame;
+	private AdventureOrganismPanel playerPanel;
+	private AdventureOrganismPanelC computerPanel;
+	private AdventureActionPanel actionPanel;
+	private static final int HUMAN = 0;
+	private static final int AI = 1;
+	private int turn;
 }
