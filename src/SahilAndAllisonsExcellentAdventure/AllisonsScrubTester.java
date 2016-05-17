cfa6ef71e8c55a3d852c5a027311b9ea11497a35
Allison <weiallison1999@gmail.com>
2016-05-17 10:24:13 -0400

    oh NO

---
+++ src/SahilAndAllisonsExcellentAdventure/AllisonsScrubTester.java
@@ -0,0 +1,12 @@
+package SahilAndAllisonsExcellentAdventure;
+
+import javax.swing.JFrame;
+
+public class AllisonsScrubTester {
+	public static void main(String args[]){
+		JFrame yo = new JFrame();
+		yo.setContentPane(new AdventureOrganismPanel());
+		yo.pack();
+		yo.setVisible(true);
+	}
+}
