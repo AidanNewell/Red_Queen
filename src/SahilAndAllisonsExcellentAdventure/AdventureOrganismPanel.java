3542587ce39b44a27f70ed54fbe931f705963a3b
Allison <weiallison1999@gmail.com>
2016-05-17 15:29:29 -0400

    y can't i use image path m8

--- src/SahilAndAllisonsExcellentAdventure/AdventureOrganismPanel.java
+++ src/SahilAndAllisonsExcellentAdventure/AdventureOrganismPanel.java
@@ -4,25 +4,42 @@ import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
-
 import javax.swing.*;
+import cards.Card;
 public class AdventureOrganismPanel extends JPanel{
-	private ArrayList<JButton> cardButtons = new ArrayList<JButton>();
-	private ImageIcon test = new ImageIcon("assets/Cytoplasm.png");
-	private int place = 0;
+	private ArrayList<JButton> cardButtons;
+	private ArrayList<Card> cards;
 	private AdventureGame g;
 	AdventureOrganismPanel(AdventureGame G){
-		
+		cardButtons = new ArrayList<JButton>();
+		cards = new ArrayList<Card>();
 		g=G;
-		
 		this.setLayout(new GridLayout(4,3));
 		for(int col=0;col<3;col++){
 			for(int row=0;row<4;row++){
-				JButton card = new JButton("" + place);
-				cardButtons.add(card);
-				this.add(card);
-				place++;
+				JButton button = new JButton();
+				cardButtons.add(button);
+				this.add(button);
 			}
+		}
+	}
+	public void addCard(Card c){
+		if(cards.size()<12){
+			cards.add(c);
+			System.out.println("Added Card");
+		}
+		repaintGrid();
+	}
+	public void removeCard(Card c){
+		if(cards.contains(c))
+			cards.remove(c);
+		repaintGrid();
+	}
+	public void repaintGrid(){
+		for(int i=0;i<cards.size();i++){
+			System.out.println(cards.get(i));
+			ImageIcon cardArt = new ImageIcon(cards.get(i).getCardArt());
+			cardButtons.get(i).setIcon(cardArt);
 		}
 	}
 }
--- src/SahilAndAllisonsExcellentAdventure/AllisonsScrubTester.java
+++ src/SahilAndAllisonsExcellentAdventure/AllisonsScrubTester.java
@@ -1,12 +1,38 @@
 package SahilAndAllisonsExcellentAdventure;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+import cards.*;
 public class AllisonsScrubTester {
-	public static void main(String args[]){
+	public static void lol(){
 		JFrame yo = new JFrame();
-		yo.setContentPane(new AdventureOrganismPanel());
+		AdventureGame g = new AdventureGame();
+		AdventureOrganismPanel testPanel = new AdventureOrganismPanel(g);
+		testPanel.addCard(new CytosolCard());
+		testPanel.addCard(new MitochondriaCard());
+		yo.setContentPane(testPanel);
 		yo.pack();
 		yo.setVisible(true);
+	}
+	public static void haha(){
+		JFrame yo = new JFrame();
+		JPanel hello = new JPanel();
+		JButton plz = new JButton();
+		CytosolCard crd = new CytosolCard();
+		//ImageIcon icon = new ImageIcon("assets/Cytoplasm.png");
+		//Icon icon = (Icon) crd.getCardArt();
+		//plz.setIcon((Icon)crd.getCardArt());
+		plz.setIcon(new ImageIcon(crd.getCardArt()));
+		hello.add(plz);
+		yo.setContentPane(hello);
+		yo.pack();
+		yo.setVisible(true);
+	}
+	public static void main(String args[]){
+		haha();
 	}
 }
