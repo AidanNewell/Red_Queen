4a1f9f167908a262ff9ff99dd88c4b88bea1bae3
Allison Wei <weiallison1999@gmail.com>
2016-05-17 12:53:47 -0400

    ok

---
+++ src/SahilAndAllisonsExcellentAdventure/adventureCardButton.java
@@ -0,0 +1,10 @@
+package SahilAndAllisonsExcellentAdventure;
+import javax.swing.*;
+import cards.Card;
+
+public class adventureCardButton extends JButton{
+	private Card c;
+	public adventureCardButton(Card card){
+		c = card;
+	}
+}
