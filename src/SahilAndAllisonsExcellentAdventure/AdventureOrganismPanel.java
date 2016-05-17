76d1489a7e9d83de801bddbcb4d2c9a38e6a0422
Allison <weiallison1999@gmail.com>
2016-05-17 18:15:33 -0400

    Plz. git plz.

--- src/SahilAndAllisonsExcellentAdventure/AdventureOrganismPanel.java
+++ src/SahilAndAllisonsExcellentAdventure/AdventureOrganismPanel.java
@@ -1,24 +1,43 @@
 package SahilAndAllisonsExcellentAdventure;
 
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
+
 import javax.swing.*;
-import cards.Card;
+
+import client.*;
+import data.*;
+import cards.*;
+import data.ImagePath;
 public class AdventureOrganismPanel extends JPanel{
-	private ArrayList<JButton> cardButtons;
+	private ArrayList<adventureCardButton> cardButtons;
 	private ArrayList<Card> cards;
 	private AdventureGame g;
+	private int selectedIndex;
 	AdventureOrganismPanel(AdventureGame G){
-		cardButtons = new ArrayList<JButton>();
+		cardButtons = new ArrayList<adventureCardButton>();
 		cards = new ArrayList<Card>();
 		g=G;
+		int count = 0;
 		this.setLayout(new GridLayout(4,3));
 		for(int col=0;col<3;col++){
 			for(int row=0;row<4;row++){
-				JButton button = new JButton();
+				final adventureCardButton button = new adventureCardButton(count);
+				count++;
+				button.setPreferredSize(new Dimension(170,170));
 				cardButtons.add(button);
+				button.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent e)
+					{
+						selectedIndex = button.getIndex();
+					}
+				});
+				
 				this.add(button);
 			}
 		}
@@ -26,7 +45,6 @@ public class AdventureOrganismPanel extends JPanel{
 	public void addCard(Card c){
 		if(cards.size()<12){
 			cards.add(c);
-			System.out.println("Added Card");
 		}
 		repaintGrid();
 	}
@@ -36,10 +54,15 @@ public class AdventureOrganismPanel extends JPanel{
 		repaintGrid();
 	}
 	public void repaintGrid(){
-		for(int i=0;i<cards.size();i++){
-			System.out.println(cards.get(i));
+		for(int i=0;i<cards.size();i++){ 
 			ImageIcon cardArt = new ImageIcon(cards.get(i).getCardArt());
 			cardButtons.get(i).setIcon(cardArt);
 		}
+		for(int i=cards.size();i<12;i++){
+			cardButtons.get(i).setIcon(null);
+		}
+	}
+	public Card getSelectedCard(){
+		return cards.get(selectedIndex);
 	}
 }
--- src/SahilAndAllisonsExcellentAdventure/adventureCardButton.java
+++ src/SahilAndAllisonsExcellentAdventure/adventureCardButton.java
@@ -3,8 +3,11 @@ import javax.swing.*;
 import cards.Card;
 
 public class adventureCardButton extends JButton{
-	private Card c;
-	public adventureCardButton(Card card){
-		c = card;
+	private int index;
+	public adventureCardButton(int i){
+		index = i;
+	}
+	public int getIndex(){
+		return index;
 	}
 }
