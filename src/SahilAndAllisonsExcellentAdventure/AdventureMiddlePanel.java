b56b30f6db53f5e4b5cd4dff51f92099422a18fd
Sahil Saini <sssaini011@gmail.com>
2016-05-17 20:56:34 -0400

    for the fourth time now

--- src/SahilAndAllisonsExcellentAdventure/AdventureMiddlePanel.java
+++ src/SahilAndAllisonsExcellentAdventure/AdventureMiddlePanel.java
@@ -5,32 +5,41 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 import cards.*;
+
 public class AdventureMiddlePanel extends JPanel{
+	
 	private AdventureGame g;
 	private AdventureOrganismPanel p;
 	private final int CYTOPLASM = 0;
 	private final int ORGANELLE = 1;
 	private adventureCardButton cyto;
 	private adventureCardButton petri;
+	
 	public AdventureMiddlePanel(AdventureGame G, AdventureOrganismPanel P){
+		
 		g=G;
 		p=P;
+		
 		cyto = new adventureCardButton(0);
 		cyto.setIcon(new ImageIcon("assets/CytoplasmBack.png"));
+		
 		cyto.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent arg0) {
 				Card cytoCard = CardLoader.getCytoplasmCard();
 				p.addCard(cytoCard);
 			}
 		});
+		
 		petri = new adventureCardButton(1);
 		petri.setIcon(new ImageIcon("assets/PetriDishBack.png"));
+		
 		petri.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent arg0) {
 				Card petriCard = CardLoader.getPetriCard();
 				p.addCard(petriCard);
 			}
 		});
+		
 		this.setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
 		this.add(cyto);
 		this.add(petri);
