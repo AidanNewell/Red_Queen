28938981feb4d2579c13594a5d1fb254b7599e56
Allison Wei <weiallison1999@gmail.com>
2016-09-19 13:16:25 -0400

    many class

---
+++ src/Yahtzee/ChanceCombination.java
@@ -0,0 +1,28 @@
+package Yahtzee;
+
+public class ChanceCombination extends AbstractYahtzeeCombination{
+	@Override
+	public int score(int[] dice) {
+		int score = 0;
+		for(int i = 0; i < dice.length; i++){
+			score+= dice[i];
+		}
+		return score;
+	}
+
+	@Override
+	public String name() {
+		return "Change Combination";
+	}
+
+	@Override
+	public String description() {
+		return "Adds all dice";
+	}
+
+	@Override
+	public boolean upperSection() {
+		return false;
+	}
+
+}
---
+++ src/Yahtzee/ThreesCombination.java
@@ -0,0 +1,30 @@
+package Yahtzee;
+
+public class ThreesCombination extends AbstractYahtzeeCombination{
+
+	@Override
+	public int score(int[] dice) {
+		int score = 0;
+		for(int i = 0; i < dice.length; i++){
+			if (dice[i] == 3)
+				score+=3;
+		}
+		return score;
+	}
+
+	@Override
+	public String name() {
+		return "Threes Combination";
+	}
+
+	@Override
+	public String description() {
+		return "Adds up sum of all dice with the number 3";
+	}
+
+	@Override
+	public boolean upperSection() {
+		return true;
+	}
+
+}
