52d68e27a8966009e036a9bd88a6ed3d4eb623a6
Aidan Newell <am.newell@verizon.net>
2016-09-19 12:54:14 -0400

    What's in the package

---
+++ src/Yahtzee/AbstractYahtzeeCombination.java
@@ -0,0 +1,25 @@
+package Yahtzee;
+
+public abstract class AbstractYahtzeeCombination {
+
+	private AbstractYahtzeeCombination;
+	
+	
+	public abstract int score (int [] dice);
+	
+	public abstract String name();
+	
+	public abstract String description();
+	
+	public abstract boolean upperSection();
+	
+	public static AbstractYahtzeeCombination[] allCombinations()
+	{
+		return {
+	}
+		
+	public static int combinationIndex(String combinationName)
+	{
+		
+	}
+}
