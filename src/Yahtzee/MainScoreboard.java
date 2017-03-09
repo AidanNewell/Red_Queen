1014f0dd550c72ac6bf633ceeeeac207ee844445
Allison Wei <weiallison1999@gmail.com>
2017-03-09 13:05:09 -0500

    have fun

--- src/Yahtzee/MainScoreboard.java
+++ src/Yahtzee/MainScoreboard.java
@@ -15,7 +15,7 @@ public class MainScoreboard {
 
 		Strategy[] stratList = {new YahtzeeTestStrategy(),new GreedyYahtzeeStrategy(), new BetterStrategyOne(), 
 				new BetterStrategyTwo(), new BetterStrategyThree(), new BetterStrategyFour(), new BetterStrategyFive(), 
-				new Strategy11(), new Strategy12(), new Strategy21(), new Strategy22(), new Strategy23(), new Strategy33()};
+				new Strategy11(), new Strategy12(), new Strategy21(), new Strategy22(), new Strategy23(), new Strategy24()};
 
 		
 		window.setStrategies(stratList);
